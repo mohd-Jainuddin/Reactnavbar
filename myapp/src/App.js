cbbf40c151cd11d211e11fa32b03cf189deeaa7a
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2022-11-07 18:10:37 +0530

    Navbar

---
+++ myapp/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="Left">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqVoNt1cV69HL0jw7TNV1P4du6KUjgaD9hqg&usqp=CAU"
+          alt="logo"
+        />
+      </div>
+
+      <div className="Right">
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>GALLERY</li>
+          <li>CONTACT</li>
+          <li>OTHERS</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
